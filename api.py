6a345575246313bc9587f8c31823b319141b54ef
Ariel Núñez <ingenieroariel@gmail.com>
2010-06-05 20:37:33 -0400

    Refactored to use the new api

--- api.py
+++ api.py
@@ -1,21 +1,10 @@
-import ogr
-from shapely.wkb import loads
-import urllib
-import os
-import tempfile
 from owslib.wfs import WebFeatureService
 from owslib.wcs import WebCoverageService
 import geojson
 
-GEOSERVER_BASE_URL = "http://localhost:8080/geoserver/"
-WFS_URL = GEOSERVER_BASE_URL + "wfs?"
-POLITICAL_LAYER = "testing:district"
-
-def get_layer(layer=POLITICAL_LAYER):
-    wfs = WebFeatureService(WFS_URL, version='1.0.0')
+def get_features(wfs_url, layer):
+    wfs = WebFeatureService(wfs_url, version='1.0.0')
     if layer not in wfs.contents.keys():
         return None
     response = wfs.getfeature(typename=[layer], format="json")
     return geojson.loads(response.read())
-
-print get_layer()
--- demo.py
+++ demo.py
@@ -1,35 +1,14 @@
 """This is an example of using the Geoserver API
 """
-
-from api import Catalog, download_shapefile, read_shapefile, download_rasterfile
-import os
+from api import get_features
 
 GEOSERVER_BASE_URL = "http://localhost:8080/geoserver/"
-WFS_SHP_BASE_URL = GEOSERVER_BASE_URL + "ows?service=WFS&version=1.0.0&request=GetFeature&outputFormat=SHAPE-ZIP&typeName="
+WFS_URL = GEOSERVER_BASE_URL + "wfs?"
 
-#WFS_RASTER_BASE_URL = GEOSERVER_BASE_URL + 
-#http://localhost:8080/geoserver/wms?service=WMS&version=1.1.0&request=GetMap&layers=Indonesia:rastert_ispop201&styles=&bbox=91.57,-13.155,146.112,7.32&width=879&height=330&srs=EPSG:4326&format=image/tiff
-
-
-POLITICAL_LAYER = "district"
+POLITICAL_LAYER = "testing:district"
 POPULATION_LAYER = "rastert_ispop201"
 
+data = get_features(wfs_url=WFS_URL, layer=POLITICAL_LAYER)
 
-
-
-catalog = Catalog(GEOSERVER_BASE_URL + "rest")
-
-# FIXME (Ole): I would like to be able to determine if the layer is vector or raster
-
-# Get the vector layer
-layer = catalog.get_layer(POLITICAL_LAYER)
-print layer.name
-tmpdir, basename = download_shapefile(WFS_SHP_BASE_URL+layer.name)
-print tmpdir, basename
-read_shapefile(os.path.join(tmpdir, basename + '.shp'))
-
-# Get the raster layer
-#layer = catalog.get_layer(POPULATION_LAYER)
-#download_rasterfile(WFS_BASE_URL+layer.name)
-
-
+print "CRS: ", data["crs"]
+print "Number of features:", len(data["features"])
