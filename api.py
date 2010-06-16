e91d95c760d671d3f022579456d9499ca924fba6
Robyn and Ole <ro@desktop.(none)>
2010-06-16 11:07:58 +0700

    Change demo to work on test server at AIFDR

--- api.py
+++ api.py
@@ -2,11 +2,14 @@ from owslib.wfs import WebFeatureService
 from owslib.wcs import WebCoverageService
 import geojson
 
-def get_features(wfs_url, layer):
+def get_features(wfs_url, layer, verbose=False):
+
+    if verbose:
+        print('Retrieving %s from %s' % (layer, wfs_url))
     wfs = WebFeatureService(wfs_url, version='1.0.0')
     if layer not in wfs.contents.keys():
         return None
-    response = wfs.getfeature(typename=[layer], format="json")
+    response = wfs.getfeature(typename=[layer], format='json')
     return geojson.loads(response.read())
 
 def get_coverage(wcs_url, layer):
@@ -14,5 +17,5 @@ def get_coverage(wcs_url, layer):
     if layer not in wcs.contents.keys():
         return None
 
-    response = wcs.getcoverage(typename=[layer], format="GeoTIFF")
+    response = wcs.getcoverage(typename=[layer], format='GeoTIFF')
     return response
--- demo.py
+++ demo.py
@@ -2,18 +2,18 @@
 """
 from api import get_features, get_coverage
 
-GEOSERVER_URL = "http://localhost:8080/geoserver/ows?"
+GEOSERVER_URL = 'http://www.aifdr.org:8080/geoserver/ows?'
 
-POLITICAL_LAYER = "testing:district"
-POPULATION_LAYER = "population"
+POLITICAL_LAYER = 'test:gadm_IDN_1'
+POPULATION_LAYER = 'test:gazette'
 
-data = get_features(wfs_url=GEOSERVER_URL, layer=POLITICAL_LAYER)
+data = get_features(wfs_url=GEOSERVER_URL, layer=POLITICAL_LAYER, verbose=True)
 
 # This data is in the GEOJSON format. We need to think about a workable
 # Python structure that will allow access to the numerical data as
 # a consecutive array
-print "CRS: ", data["crs"]
-print "Number of features:", len(data["features"])
+print 'CRS: ', data['crs']
+print 'Number of features:', len(data['features'])
 
 #raster_data = get_coverage(wcs_url=GEOSERVER_URL, layer=POPULATION_LAYER)
 #print raster_data
